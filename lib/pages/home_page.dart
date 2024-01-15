4b8b26b78431bee620f657e6dffc02eeed648d84
terminatedNULL <135302972+terminatedNULL@users.noreply.github.com>
2024-01-14 22:14:48 -0500

    Aded spinning background to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,18 @@
+//Flutter imports
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+//Backend imports
 import 'package:random_animal_parts_site/classes.dart';
 import 'package:random_animal_parts_site/globals.dart' as globals;
-import 'package:random_animal_parts_site/components/bw_button.dart';
+
+//Page imports
 import 'package:random_animal_parts_site/pages/about_page.dart';
+
+//Component imports
+import 'package:random_animal_parts_site/components/infinite_animation.dart';
+import 'package:random_animal_parts_site/components/bw_button.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,18 +20,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("lib/images/sunburst_dense.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
+      body: Center(
           child: Stack(
             alignment: AlignmentDirectional.topCenter,
             children: <Widget>[
 
+              //Spinning background
+              Center(
+                child: InfiniteAnimation(
+                  durationInSeconds: 90,
+                  child: Transform.scale(
+                    scale: 4,
+                    child: SvgPicture.asset("lib/images/sunburst_dense_vector.svg"),
+                  ),
+                ),
+              ),
+
+              //Center Logo
               Center(
                 child: Container(
                   width: 300,
@@ -33,55 +48,55 @@ class HomePage extends StatelessWidget {
                       color: Colors.black,
                       width: 6,
                     ),
-                    image: const DecorationImage(
-                      image: AssetImage("lib/images/leviathan_cross.png"),
-                      scale: 9.5,
-                      //TODO : Find a better fix for this image's centering
-                      alignment: AlignmentDirectional(0, -0.25),
+                  ),
+                  child: Transform.scale(
+                    scale: 0.85,
+                    alignment: const AlignmentDirectional(0, -0.75),
+                    child: SvgPicture.asset(
+                      "lib/images/leviathan_cross.svg",
                     ),
                   ),
-                  child: null,
                 ),
               ),
 
-              Column(
+              //Header
+              const Column(
                 children: [
 
-                  const SizedBox(
+                  SizedBox(
                     height: 25,
                   ),
 
-                  SizedBox(
-                    width: 450,
-                    height: 65,
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: const BorderRadius.all(Radius.circular(10)),
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 2,
-                        ),
-                      ),
-                      child: const Center(
-                        child: Text(
-                          "random animal parts",
-                          style: TextStyle(
-                              fontFamily: 'Langdon',
-                              fontSize: 50,
-                          )
-                        ),
+                  //Text Logo
+                  Center (
+                    child: Expanded(
+                      child: Row(
+                        children: [
+                          SizedBox(width: 100),
+
+                          Text(
+                              "random animal parts",
+                              style: TextStyle(
+                                fontFamily: 'Langdon',
+                                fontSize: 70,
+                              )
+                          ),
+
+                          SizedBox(width: 100),
+                        ],
                       ),
                     ),
                   ),
 
-                  const SizedBox(
+                  SizedBox(
                     height: 25,
                   ),
 
-                  const Row(
+                  //Page Buttons
+                  Row(
                     children: [
-                      BW_Button(text: "About Us", loadPage: AboutPage(),),
+                      SizedBox(width: 25),
+                      BWButton(text: "About Us", width: 100, height: 35, loadPage: AboutPage(),),
                     ],
                   )
 
@@ -91,7 +106,6 @@ class HomePage extends StatelessWidget {
             ]
           ),
         ),
-      ),
     );
   }
 }
